a1b95c2eca25a6823fbffddfe03408aaaa989d7d
Oliver <oliver@tapirens.se>
2020-09-11 22:35:28 +0200

    feat(Page): add option to show page in footer

--- mysilverstripeapp/app/src/Page.php
+++ mysilverstripeapp/app/src/Page.php
@@ -3,12 +3,23 @@
 namespace {
 
     use SilverStripe\CMS\Model\SiteTree;
+    use SilverStripe\Forms\CheckboxField;
 
-    class Page extends SiteTree
+class Page extends SiteTree
     {
-        private static $db = [];
+        private static $db = [
+            'ShowInFooter' => 'Boolean',
+        ];
 
         private static $has_one = [];
+
+        public function getCMSFields() {
+            $fields = parent::getCMSFields();
+
+            $fields->addFieldToTab('Root.Settings', CheckboxField::create('ShowInFooter'), 'ShowInSearch');
+
+            return $fields;
+        }
 
         // Return all testimonials
         public function getTestimonials() {
